11aa85cc51d0cd400931890e5582548ba5348602
yasarkilicx <yasarkilicx@gmail.com>
2023-09-04 16:53:04 +0300

    Initial Commit

---
+++ DiyetYemekler/DiyetYemeklerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiyetYemeklerApp.swift
+//  DiyetYemekler
+//
+//  Created by yasarkilic on 4.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct DiyetYemeklerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
